0c230cc5441c39f17d8198bf117b45155f245869
victorgri <vsgrishanin@edu.hse.ru>
2021-10-25 03:50:15 +0300

    fix

--- hw_asr/augmentations/wave_augmentations/__init__.py
+++ hw_asr/augmentations/wave_augmentations/__init__.py
@@ -1,9 +1,9 @@
 from hw_asr.augmentations.wave_augmentations.Gain import Gain
 from hw_asr.augmentations.wave_augmentations.Noise import Noise
-from hw_asr.augmentations.wave_augmentations.Shift import Pitch
+from hw_asr.augmentations.wave_augmentations.Shift import Shift
 
 __all__ = [
     "Gain",
     "Noise",
-    "Shift.py"
+    "Shift"
 ]
